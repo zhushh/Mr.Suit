ced0532e14d4b29cbda3671e32c4a28ae4aed644
william <1244837903@qq.com>
2015-06-01 09:15:02 +0800

    修改

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,5 +1,10 @@
 Template.postsList.helpers({
   posts: function() {
-    return Meteor.users.find({"profile.design": {$exists: true}});
+    var str = Session.get("isSearch");
+    if (str == "") {
+      return Meteor.users.find({"profile.design": {$exists: true}});
+} else {
+      return Meteor.users.find({ '$or' : [{"profile.design.creator" : str}, {"profile.design.title" : str}]});
+}
   }
 });
